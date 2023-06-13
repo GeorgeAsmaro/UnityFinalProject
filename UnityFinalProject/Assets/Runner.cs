b8bb134cf6912b23155df372d8f9eb420cd7e4bc
George Asmaro <67618081+GeorgeAsmaro@users.noreply.github.com>
2023-06-13 14:51:57 -0700

    added a ui system
    15 second freeze time and the color starts red, slowly transitions to yellow then to green and when it's full green you can move. also added a ui system for roundtime of 2 minutes but i made it so that for example 2 minutes looks like 2:00 and then it goes 1:59 all the way till 0:00 which is when the round will end, and when the round time hits 0:10 the text turns red to signify that there is not much time left in the round

--- UnityFinalProject/Assets/Runner.cs
+++ UnityFinalProject/Assets/Runner.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Runner : MonoBehaviour
 {
     float timer = 15;
+    float goTimer = 3;
     float speedToSet;
+    float roundTimer = 120;
     GameObject player1;
+    public TMP_Text freezeTime;
+    public TMP_Text roundTime;
+    public Color startColor = Color.red;
+    public Color middleColor = Color.yellow;
+    public Color endColor = Color.green;
 
     void Start()
     {
@@ -16,17 +25,77 @@ public class Runner : MonoBehaviour
 
     void Update()
     {
+        if (timer > 0)
+        {
+            roundTime.text = "";
+        }
+
         timer -= Time.deltaTime;
+        string num;
+        if (timer >= 10)
+        {
+            num = timer.ToString().Substring(0, 4);
+        }
+        else
+        {
+            num = timer.ToString().Substring(0, 3);
+        }
+        freezeTime.text = num;
 
         if (timer > 0)
         {
             player1.GetComponent<PlayerMovement>().setSpeed(0);
         }
 
-        if (timer < 0)
+        if (timer <= 0)
         {
             player1.GetComponent<PlayerMovement>().setSpeed(speedToSet);
+            timer = 0;
+            freezeTime.text = "GO!";
+            goTimer -= Time.deltaTime;
+
+            roundTimer -= Time.deltaTime;
+
+            if(roundTimer <= 0)
+            {
+                roundTimer = 0;
+            }
+
+            if((int)roundTimer % 60 > 9)
+            {
+                roundTime.text = ((int)roundTimer / 60) + ":" + ((int)roundTimer % 60);
+
+            }
+            else
+            {
+                roundTime.text = ((int)roundTimer / 60) + ":0" + ((int)roundTimer % 60);
+            }
+
+            if (goTimer <= 0)
+            {
+                freezeTime.enabled = false;
+            }
         }
+
+        if(roundTimer <= 11)
+        {
+            roundTime.color = startColor;
+        }
+
+        // Calculate the color based on timer value
+        float t = Mathf.Clamp01(1 - (timer / 15f));
+        Color lerpedColor = Color.Lerp(startColor, endColor, t);
+
+        if (t < 0.5f)
+        {
+            lerpedColor = Color.Lerp(startColor, middleColor, t * 2);
+        }
+        else
+        {
+            lerpedColor = Color.Lerp(middleColor, endColor, (t - 0.5f) * 2);
+        }
+
+        // Apply the color to the text
+        freezeTime.color = lerpedColor;
     }
 }
-
